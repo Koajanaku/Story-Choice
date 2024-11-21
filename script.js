f58f2f74f205fa568357df164189509ce3b596e7
Koajanaku <koajanaku@cps.edu>
2024-11-21 15:38:29 +0000

    added some images

---
+++ script.js
@@ -0,0 +1,7 @@
+let left =  document.querySelector('.left');
+let right =  document.querySelector('.right');
+let leftRight =  document.querySelector('.leftRight');
+let text =  document.querySelector('p');
+let Alley =  document.querySelector('.left');
+let left =  document.querySelector('.left');
+
