4f723ce00841a72169fec46b63c812c0a391e891
Koajanaku <koajanaku@cps.edu>
2024-12-02 15:32:38 +0000

    figured out ting

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ let right =  document.querySelector('.right');
 let leftRight =  document.querySelector('.leftRight');
 let text =  document.querySelector('p');
 let Alley =  document.querySelector('.left');
-let left =  document.querySelector('.left');
+
+?.style.display.none
 
