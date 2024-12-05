294ecfcffb855d2d51f66a36ab4a99c719ad2155
Koajanaku <koajanaku@cps.edu>
2024-12-05 15:40:14 +0000

    999

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 let left =  document.querySelector('.left');
+let leftRight =  document.querySelector('.');
+let leftRightend =  document.querySelector('.leftRight');
+let leftLeft =  document.querySelector('.leftRight');
+let leftLeftEnd =  document.querySelector('.');
+
 let right =  document.querySelector('.right');
-let leftRight =  document.querySelector('.leftRight');
+let rightRight =  document.querySelector('.');
+let rightRightEnd =  document.querySelector('.leftRight');
+let rightLeft =  document.querySelector('.leftRight');
+let rightLeftEnd =  document.querySelector('.');
+
 let text =  document.querySelector('p');
 let Alley =  document.querySelector('.left');
 
-?.style.display.none
+left.addEventListener('click', function(){
+});
+leftRight.addEventListener('click', function(){
+});
+leftRightEnd.addEventListener('click', function(){
+});
+leftLeft.addEventListener('click', function(){
+});
+leftLeftEnd.addEventListener('click', function(){
+});
+right.addEventListener('click', function(){
+});
+rightRight.addEventListener('click', function(){
+});
+rightRightEnd.addEventListener('click', function(){
+});
+rightLeft.addEventListener('click', function(){
+});
+rightLeftEnd.addEventListener('click', function(){
+});
 
