50d767f74a08a41f8614748653851fdd0c9a4796
Koajanaku <koajanaku@cps.edu>
2024-12-09 15:40:29 +0000

    fixable

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let left =  document.querySelector('.left');
-let leftRight =  document.querySelector('.');
+let leftRight =  document.querySelector('.leftRight');
 let leftRightend =  document.querySelector('.leftRight');
 let leftLeft =  document.querySelector('.leftRight');
-let leftLeftEnd =  document.querySelector('.');
+let leftLeftEnd =  document.querySelector('.leftLeftEnd');
 
 let right =  document.querySelector('.right');
-let rightRight =  document.querySelector('.');
+let rightRight =  document.querySelector('.rightRight');
 let rightRightEnd =  document.querySelector('.leftRight');
 let rightLeft =  document.querySelector('.leftRight');
-let rightLeftEnd =  document.querySelector('.');
+let rightLeftEnd =  document.querySelector('.rightLeftEnd');
 
 let text =  document.querySelector('p');
 let Alley =  document.querySelector('.left');
+
+leftLeft.style.display="none";
+leftLeftEnd.style.display="none";
+leftRight.style.display="none";
+leftRightEnd.style.display="none";
+rightRightLeft.style.display= "none";
+rightRightEnd.style.display= "none";
+rightLeft.style.display= "none";
+rightLeftEnd.style.display= "none";
 
 left.addEventListener('click', function(){
 });
